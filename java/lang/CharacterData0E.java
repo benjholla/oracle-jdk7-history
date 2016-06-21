97a98fad3b8776dd91312b76a11dc4f3efc7cb12
Ben Holland <benjholla@gmail.com>
2016-06-21 12:26:22 -0500

    jdk-7u5-linux-x64

--- java/lang/CharacterData0E.java
+++ java/lang/CharacterData0E.java
@@ -1,4 +1,4 @@
-// This file was generated AUTOMATICALLY from a template file Thu Apr 12 01:00:44 GMT-08:00 2012
+// This file was generated AUTOMATICALLY from a template file Wed Jun 27 00:12:14 PDT 2012
 /*
  * Copyright (c) 2003, 2006, Oracle and/or its affiliates. All rights reserved.
  * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
@@ -241,7 +241,7 @@ class CharacterData0E extends CharacterData {
     private CharacterData0E() {};
 
     // The following tables and code generated using:
-  // java GenerateCharacter -plane 14 -template ../../tools/GenerateCharacter/CharacterData0E.java.template -spec ../../tools/UnicodeData/UnicodeData.txt -specialcasing ../../tools/UnicodeData/SpecialCasing.txt -proplist ../../tools/UnicodeData/PropList.txt -o /HUDSON/workspace/jdk7u4-2-build-linux-amd64-product/jdk7u4/build/linux-amd64/gensrc/java/lang/CharacterData0E.java -string -usecharforbyte 11 4 1
+  // java GenerateCharacter -plane 14 -template ../../tools/GenerateCharacter/CharacterData0E.java.template -spec ../../tools/UnicodeData/UnicodeData.txt -specialcasing ../../tools/UnicodeData/SpecialCasing.txt -proplist ../../tools/UnicodeData/PropList.txt -o /HUDSON/workspace/jdk7u5-2-build-linux-amd64-product/jdk7u5/build/linux-amd64/gensrc/java/lang/CharacterData0E.java -string -usecharforbyte 11 4 1
   // The X table has 2048 entries for a total of 4096 bytes.
 
   static final char X[] = (
